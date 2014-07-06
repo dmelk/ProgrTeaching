e90508c1b052e486d1cd3c4609d7d92d7cc76f91
Melk <me@melk.kiev.ua>
2014-07-06 21:39:18 +0300

    configured vendor bundles (except FOS User)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,19 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // Sonata bundles
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
+            // FOS bundles
+//            new \FOS\UserBundle\FOSUserBundle(),
+
+            // Melk bundles
+            new Melk\ProgrTeach\MainBundle\MelkProgrTeachMainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
